2896255db7a1303c77ee20792a0f0e56023bfcb3
ElisaGDev <elisa.garet@gmail.com>
2022-06-27 18:57:55 +0200

    Creation des routes, controllers, models user

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,11 +1,15 @@
+//Importation de mongoose, unique validator
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
+//Schéma pour l'email et le mot de passe avec email unique
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
+//Application d'uniqueValidator sur le schéma
 userSchema.plugin(uniqueValidator);
 
+//Exportation du modèle
 module.exports = mongoose.model("User", userSchema);
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,9 +1,15 @@
+//Importation d'express
 const express = require("express");
+
+//Permets de créer des routes individuelles
 const router = express.Router();
 
+//Importation des dépendances nécessaires
 const userCtrl = require("../controllers/user");
 
+//Routes pour signup et login
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
 
+//Exportation du modèle
 module.exports = router;
